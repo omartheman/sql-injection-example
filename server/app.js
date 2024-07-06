bafba1880d2c66a36eb8f39c0549b0e8eb6ce4b7
Omar Shishani <omarshishanischool@gmail.com>
2024-07-05 19:21:15 -0700

    Submit form with JS without refresh; Fix CORS

--- server/app.js
+++ server/app.js
@@ -1,8 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+function setCorsHeaders(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500/');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+}
+
+app.use(setCorsHeaders);
 
 app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.post('/', (req, res) => {
+    console.log('POST request received to "/"')
     res.send('Hello world!')
 })
 
