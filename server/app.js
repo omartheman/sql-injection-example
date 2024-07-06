d6ff327b5b6da6ae3fba2d1276c1d7cc8a98907f
Omar Shishani <omarshishanischool@gmail.com>
2024-07-06 06:32:12 -0700

    Add body-parser but still fail to read body

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.urlencoded({ extended: false }))
 
 function setCorsHeaders(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500/');
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
@@ -17,6 +19,7 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     console.log('POST request received to "/"')
+    console.log('req.body:',req.body)
     res.send('Hello world!')
 })
 
